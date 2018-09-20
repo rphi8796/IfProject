61f3f98a20520af95043f8a27ac3b9b7b7306d59
rphi8796 <jlkfop@gmail.com>
2018-09-20 13:09:49 -0600

    I added a lot of JOptionPane messages and input and then I started to add some defensive code

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -2,22 +2,76 @@ package controller;
 
 import model.WaterBottle;
 
-
+import javax.swing.JOptionPane;
 
 public class IfController
 {
 	private WaterBottle hydroFlask;
 	private WaterBottle userWaterBottle;
 	
+	
+	/**
+	 * Builds of the Controller
+	 */
 	public IfController()
 	{
 		hydroFlask = new WaterBottle("Grey", "Straw", "Metal", 40, true);
 		userWaterBottle = new WaterBottle();
 	}
 	
-	
+	/**
+	 * This is where the program starts calling methods to run the program.
+	 */
 	public void start()
 	{
+		String color = JOptionPane.showInputDialog(null, "What is the color of your water bottle?");
+		userWaterBottle.setColor(color);
+		JOptionPane.showMessageDialog(null, "Your water bottle has a beatiful " + userWaterBottle.getColor() + " color.");
+		
+		
+		String lydType = JOptionPane.showInputDialog(null, "What kind of lid does your water have?");
+		userWaterBottle.setLidType(lydType);
+		JOptionPane.showMessageDialog(null, "Your water bottle has a " + userWaterBottle.getLidType() + " lid");
+		
+	
+		String material = JOptionPane.showInputDialog(null, "What is a majority of your water bottle made of?");
+		userWaterBottle.setMaterial(material);
+		JOptionPane.showMessageDialog(null, "Your water bottle is made of " + userWaterBottle.getMaterial());
+		
+		
+		String userInput = JOptionPane.showInputDialog(null, "How many fluid ounces are in your water bottle?");
+		int fluidOz = 0;
+		if (validInt(userInput))
+		{
+			fluidOz = Integer.parseInt(userInput);
+		}
+		
+		userWaterBottle.setFluidOz(fluidOz);
+		JOptionPane.showMessageDialog(null, "Your water bottle holds " + userWaterBottle.getFluidOz() + " fluid ounces");
+		
+		
+		String userInput2 = JOptionPane.showInputDialog(null, "Is your water bottle insulated?");
+		boolean insulated = false;
+		insulated = Boolean.parseBoolean(userInput2);
+		userWaterBottle.setinsulated(insulated);
+	}
+			
+	public boolean validInt(String maybeInt)
+	{
+		
+		boolean isValid = false;
+			
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You need to type in a whole number");
+		}
+		
+		return isValid;
 		
 	}
 }
